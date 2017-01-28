de911d82a66960625c2de3c0dec068de9995b161
jferris72 <jferris@ualberta.ca>
2017-01-28 14:00:50 -0700

    added some ui stuff

---
+++ app/src/main/java/com/example/brianofrim/juqe/CodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.brianofrim.juqe;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CodeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code);
+    }
+}
