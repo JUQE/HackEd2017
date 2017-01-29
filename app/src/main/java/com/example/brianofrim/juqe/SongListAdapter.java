bdac543a62ba60655ec5f818b7e867aa196d7647
jferris72 <jferris@ualberta.ca>
2017-01-28 19:19:09 -0700

    Added create venue to startup page

--- app/src/main/java/com/example/brianofrim/juqe/SongListAdapter.java
+++ app/src/main/java/com/example/brianofrim/juqe/SongListAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -33,7 +34,7 @@ public class SongListAdapter extends ArrayAdapter<Song> {
         }
         TextView songName = (TextView) convertView.findViewById(R.id.songName);
         TextView songArtist = (TextView) convertView.findViewById(R.id.songArtist);
-        Button upvoteButton = (Button) convertView.findViewById(R.id.upvote_button);
+        ImageButton upvoteButton = (ImageButton) convertView.findViewById(R.id.upvote_button);
         upvoteButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
